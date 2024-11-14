6b60ced563cfcf7de85c307bf9554005bb32a8a5
Carla Lopez <calopez@student.ethz.ch>
2024-11-14 15:24:28 +0100

    one step map in complete accordance

--- extras.jl
+++ extras.jl
@@ -58,3 +58,114 @@ function euler(x_0, v_0, t::Tuple, nt::Int, epsilon)
 
     return x_t, v_t
 end
+
+function boris_impA_2(x_0::Vector, v_0::Vector, t::Tuple, nt::Int, epsilon::Float64)
+    """Implicit filtered Boris integrator"""
+    
+    # Parameters
+    (t0, tf) = t
+    h = (tf - t0) / (nt - 1)
+    
+    # Arrays to store the state
+    x_t = Array{Float64}(undef, 3, nt)
+    v_t = Array{Float64}(undef, 3, nt)
+    
+    x = x_0
+    v = v_0
+    
+    for i in 1:nt
+        # Store the position and velocity
+        x_t[:, i] = x # x^{n}
+        v_t[:, i] = v # v^{n-1/2}
+    
+        E_n = E(x)
+        B_n = B(x, epsilon)
+
+        x_c = x_center(x, v, B_n)
+        theta_n = theta(h*norm(B_n))
+        x_bar_n = x_bar(theta_n, x, x_c)
+        B_bar_n = B(x_bar_n, epsilon)
+
+
+        # Full step of the position x^{n+1}
+        x_ = x + h*phi_1(-h*B_bar_n)*v + h^2/2*Psi_pm_(h*B_n, h*B_bar_n, +1)*E_n
+    
+        function equation_imp(v_)
+
+            # At x+1 position
+            B_n_ = B(x_, epsilon)
+            x_c_ = x_center(x_, v_, B_n_)
+            theta_n_ = theta(h*norm(B_n_))
+            x_bar_n_ = x_bar(theta_n_, x_, x_c_)
+            B_bar_n_ = B(x_bar_n_, epsilon)
+
+            lhs = phi_1(h*B_bar_n_)*v_
+
+            term_1 = phi_1(-h*B_bar_n)*v 
+            term_2 = h/2*Psi_pm_(h*B_n_, h*B_bar_n_, -1)*E(x_)
+            term_3 = h/2*Psi_pm_(h*B_n, h*B_bar_n, +1)*E_n
+
+            rhs = term_1 + term_2 + term_3
+            return lhs - rhs
+        end
+        
+        v_guess = v
+        v = nlsolve(equation_imp, v_guess).zero
+        x = x_
+
+    end
+    return x_t, v_t
+    end
+
+function boris_twoPA_2(x_0::Vector, v_0::Vector, t::Tuple, nt::Int, epsilon::Float64)
+    """Implicit filtered Boris integrator"""
+    
+    # Parameters
+    (t0, tf) = t
+    h = (tf - t0) / (nt - 1)
+    
+    # Arrays to store the state
+    x_t = Array{Float64}(undef, 3, nt)
+    v_t = Array{Float64}(undef, 3, nt)
+    
+    x = x_0
+    v = v_0
+    
+    for i in 1:nt
+        # Store the position and velocity
+        x_t[:, i] = x # x^{n}
+        v_t[:, i] = v # v^{n-1/2}
+    
+        E_n = E(x)
+        B_n = B(x, epsilon)
+
+        x_c = x_center(x, v, B_n)
+        B_c = B(x_c, epsilon)
+
+        # Full step of the position x^{n+1}
+        x_ = x + h*Phi_pm(h*B_n, h*B_c, +1)*v + h^2/2*Psi_pm(h*B_n, h*B_c, +1)*E_n
+
+        function equation_twop(v_n)
+
+            B_n_ = B(x_, epsilon)
+            x_c_ = x_center(x_, v_n, B_n_)
+            B_c_ = B(x_c_, epsilon)
+
+            lhs = Phi_pm(h*B_n_, h*B_c_, -1)*v_n
+
+            term_1 = Phi_pm(h*B_n, h*B_c, +1)*v 
+            term_2 = h/2*Psi_pm(h*B_n_, h*B_c_, -1)*E(x_) 
+            term_3 = h/2*Psi_pm(h*B_n,  h*B_c, +1)*E_n
+            
+            rhs = term_1 + term_2 + term_3
+
+            return lhs - rhs
+        end
+        
+        v_guess = v
+        v = nlsolve(equation_twop, v_guess).zero
+        x = x_
+
+    end
+    return x_t, v_t
+    end
